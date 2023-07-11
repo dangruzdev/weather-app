d5e8241d081d6411d842b9e711d15736136ee18a
Dan <itsdangruzdev@gmail.com>
2023-07-11 21:40:07 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react';
+
+import './App.css';
+
+function App() {
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState('Москва');
+  const [inputTimeout, setInputTimeout] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (inputTimeout) {
+      clearTimeout(inputTimeout);
+    }
+
+    const timeout = setTimeout(() => {
+      fetchWeatherData(city);
+    }, 500);
+
+    setInputTimeout(timeout);
+
+    return () => {
+      clearTimeout(inputTimeout);
+    };
+  }, [city]);
+
+  const fetchWeatherData = (city) => {
+    fetch(`http://api.weatherapi.com/v1/current.json?key=140d46611506424ea0e141051230907&q=${encodeURIComponent(city.trim())}`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Error fetching weather data')}
+      })
+      .then(data => {
+        setWeather(data);
+        setError(null)
+      })
+      .catch(error => {
+        console.error('Error fetching weather data:', error);
+        setError('Что-то пошло не так, попробуйте снова.');
+      });
+  };
+
+  return (
+    <div className="App">
+      {/* <input className='city_input' type="text" value={city} onChange={(e) => setCity(e.target.value)} /> */}
+      {error && <p>{error}</p>}
+      {weather && (
+        <div>
+          <h1>Температура в городе <input className='city_input' type="text" value={city} onChange={(e) => setCity(e.target.value)} /> {weather.current.temp_c ? weather.current.temp_c : null }°C</h1>
+          <img className='weather_icon' src={`https:${weather.current.condition.icon}`} alt="Weather Icon" />
+        </div>
+      )}
+    </div>
+  );
+  
+}
+
+export default App;
